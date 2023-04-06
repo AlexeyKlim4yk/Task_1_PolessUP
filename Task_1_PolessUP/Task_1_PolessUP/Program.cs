46e7d0fdef6907ba1f1a315f3fecc58b3fcea399
AlexeyKlim4yk <114896207+AlexeyKlim4yk@users.noreply.github.com>
2023-04-06 08:07:31 +0300

    Add files via upload

---
+++ Task_1_PolessUP/Task_1_PolessUP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_1_PolessUP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char cov = '"';
+            Console.WriteLine($" +{cov}{cov}{cov}{cov}{cov}+" +
+                                    $"\n(| o o |)" +
+                                    $"\n |  ^  |" +
+                                    $"\n | '-' |" +
+                                    $"\n +-----+");
+        }
+    }
+}
